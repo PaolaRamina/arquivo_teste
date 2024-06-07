e6b064dc9a6a70d29c448a872ea49c8c6ab63e9c
Paola Ramina <paolaramina153@gmail.com>
2024-06-07 20:33:15 -0300

    Add files via upload

---
+++ programa_aula_teste.py
@@ -0,0 +1,43 @@
+# Bibliotecas
+import streamlit as st
+import pandas as pd  
+import matplotlib.pyplot as plt
+
+
+# Título na barra do navegador
+st.set_page_config(
+
+page_title="Teste de Site", <br>
+page_icon="🤡", <br>
+)
+
+
+# Cabeçalho
+st.header("Dados da Empresa 1")
+
+arquivo = "https://raw.githubusercontent.com/PaolaRamina/aula_teste/main/empresa1.csv" 
+dfe = pd.read_csv(arquivo, sep=';') 
+st.dataframe(dfe)
+
+"---"
+# Cabeçalho
+st.header("Gráficos")
+st.subheader("Indicadores ao longo do Tempo")
+fig, ax = plt.subplots()
+dfe.plot()
+st.pyplot(fig)
+
+st.subheader("Dispersão entre EBITDA e o Lucro Operacional")
+fig, ax = plt.subplots()
+dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional')
+st.pyplot(fig)
+
+st.subheader("Histograma do Lucro do Período")
+fig, ax = plt.subplots()
+dfe["Lucro do período"].plot(kind = 'hist')
+st.pyplot(fig)
+
+"---"
+# Cabeçalho
+st.subheader("Soma dos Valores de cada Ano")
+st.write(dfe.groupby('Ano').mean())
